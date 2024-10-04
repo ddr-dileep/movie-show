f66613fa4589733a9a2ce362d40887f7745ec4ce
Dileep <ddr.dileepraloti@gmail.com>
2024-10-04 12:43:16 +0530

    feat : actor filter/ get all

--- src/controllers/actor.controller.ts
+++ src/controllers/actor.controller.ts
@@ -5,17 +5,47 @@ import { Actor } from "../models/actor.model";
 export const actorContoller = {
   getAllActors: async (req: Request, res: Response): Promise<any> => {
     try {
-      const actors = await Actor.find();
+      const filters: any = req.query;
+
+      // Build dynamic query object for filtering actors
+      const query: any = {};
+
+      // filtering based on the available query params
+      if (filters.name) {
+        query.name = { $regex: filters.name, $options: "i" }; // Case-insensitive search for name
+      }
+      if (filters.gender) {
+        query.gender = filters.gender;
+      }
+      if (filters.nationality) {
+        query.nationality = filters.nationality;
+      }
+      if (filters.occupation) {
+        query.occupation = { $regex: filters.occupation, $options: "i" };
+      }
+      if (filters.birthdate) {
+        query.birthdate = { $gte: new Date(filters.birthdate) };
+      }
+      if (filters.height) {
+        query.height = { $gte: Number(filters.height) };
+      }
+
+      // Add more filters as needed, like awards, spouse, movies, etc.
+      if (filters.movies) {
+        query.movies = { $in: filters.movies.split(",") }; // Search for actors in any of the given movies
+      }
+
+      const actors = await Actor.find(query); // Get all actors based on the query/all
 
       res.status(200).json(
         API_RESPONSES.success({
           count: actors.length,
           actors: actors,
-          message: "Actors featched successfully",
+          message: "Actors fetched successfully",
         })
       );
     } catch (error) {
-      res.status(404).json(API_RESPONSES.error(error));
+      res.status(500).json(API_RESPONSES.error(error));
     }
   },
 };
