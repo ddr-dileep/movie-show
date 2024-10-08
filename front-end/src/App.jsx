42eaccbed561a48ae628ae7e41e4fff91fa1f6fc
Dileep <ddr.dileepraloti@gmail.com>
2024-10-08 16:34:22 +0530

    feat : added home page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./router";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <AppRoutes />
     </Router>
   );
--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { BASE_API_URL } from "../../constants/config";
+import { toast } from "react-toastify";
 
 export const Home = () => {
-  const [movies, setMovies] = React.useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -14,20 +16,49 @@ export const Home = () => {
         },
       });
       const data = await response.json();
-      setMovies(data);
+      if (data.success) {
+        setMovies(data?.data?.movies);
+        setIsLoading(false);
+        toast.success(data.message);
+      }
     };
 
     fetchMovies();
   }, []);
 
-  console.log("movies", movies);
+  const handleClickMovie = async (movieId) => {
+    toast.success(`Clicked on ${movieId}`);
+    // Add your own logic to handle movie click here
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="home-page">
+    <div className="home_page">
       <h1>Home Page</h1>
-      <p>Welcome to our website!</p>
-      <img src="https://example.com/image.jpg" alt="Example Image" />
-      <button onClick={() => {}}>Click Me</button>
+      <div className="home_page-movies">
+        <div className="home_page-movies-container">
+          {movies?.map((movie) => (
+            <div
+              key={movie.id}
+              className="home_page-movies-card"
+              onClick={() => handleClickMovie(movie?._id)}
+            >
+              <img
+                src={movie?.posterUrl}
+                alt={movie.title}
+                className="home_page-movies-card-image"
+              />
+              <div className="home_page-movies-card-footer">
+                <h2>{movie.title}</h2>
+                <p>{movie?.updatedAt?.slice(0, 4)}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
